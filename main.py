ed92e9a97edd5c0e47c33c866b7472312e8d8bd9
Max Britvin <maksbritvin@gmail.com>
2018-01-10 17:51:37 +0300

    Init project commit

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print ("Hello world")
+import os
+import os.path
+import re
+
+LANGYAGES = {
+    "Python": {
+        "extention": ".py",
+        "module": "python"
+    },
+    "VBScript": {
+        "extention": ".vbs",
+        "module": "vbscript"
+    },
+    "JavaScript": {
+        "extention": ".js",
+        "module": "javascript"
+    },
+    "Java": {
+        "extention": ".java",
+        "module": "java"
+    },
+    "C#": {
+        "extention": ".cs",
+        "module": "csharp"
+    }
+}
+
+# func for define code language by file extention
+def defineLang(fileext):
+    for k, v in LANGYAGES.items():
+        if v["extention"] == fileext:
+            return k, v["module"]
+
+print(defineLang(".py"))
